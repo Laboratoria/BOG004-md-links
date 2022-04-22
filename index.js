35316810ad89768bd5b718ec5a61dd0e83d0b033
yuliduquedealba <yduquedealba@gmail.com>
2022-04-22 12:30:19 -0500

    Ya funciona la promesa que permite leer otros archivos

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-module.exports = () => {
-  // ...
-};
+const { rejects } = require("assert");
+const fs = require("fs");
+const { resolve } = require("path");
+const path = require("path");
+// const fsPromises = require('fs').promises;
+const promesaPrueba = route => new Promise((resolve, reject) => {
+    resolve(
+        fs.promises.readFile(route, 'utf-8')
+        .then(result => {
+            // console.log('RESULTADOO', result);
+            return result
+        })
+        .catch(error => {
+            console.log('ERROOOR', error);
+        })
+    )
+});
+
+// Use fsPromises.readFile() method
+// to read the file
+// const promesaPrueba = ms => new Promise(resolve => setTimeout(resolve, ms));
+// wait(0).then(() => console.log(4));
+
+promesaPrueba(process.argv[2])
+    .then((result) => {
+        console.log("RESULT" + result);
+    })
+    .catch((error) => {
+        console.log("ERROOOOOOOOOOOOR", error);
+    });
+
+
+// console.log(process.argv[2]);
+// promesaPrueba(process.argv[2]);
+
+// module.exports = () => {
+//   // ...
+// };
