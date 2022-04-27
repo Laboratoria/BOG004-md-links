d55ab8468178cd9f772577d2c6f27e3dde88dc74
yuliduquedealba <yduquedealba@gmail.com>
2022-04-27 15:03:12 -0500

    Hasta el momento he logrado leer el archivo y extraer las url's se muestra el resultado correctamente pero no muestra bien el error en caso de que el archivo presente inconvenientes

--- index.js
+++ index.js
@@ -1,27 +1,67 @@
 const { rejects } = require("assert");
 const fs = require("fs");
-const { resolve } = require("path");
-const path = require("path");
-// const fsPromises = require('fs').promises;
-const promesaPrueba = route => new Promise((resolve, reject) => {
-    resolve(
-        fs.promises.readFile(route, 'utf-8')
+const markdownLinkExtractor = require('markdown-link-extractor');
+const axios = require('axios');
+const { resolve, normalize } = require("path");
+
+//Extrayendo URL´s del archivo
+const doHTTPRequest = function checkURL(file, url) {
+    // Make a HTTP request for a given URL
+    axios.get(url)
+        .then(function(response) {
+            console.log(file, url, response.status, response.statusText);
+            return 'axios then'
+        })
+        .catch(function(error) {
+            console.log(file, url, error.code);
+            return 'axios catch'
+
+        })
+}
+
+//Leyendo archivo
+const readingFile = route => new Promise((resolve, reject) => {
+    //resolve(
+    fs.promises.readFile(route, 'utf-8')
         .then(result => {
-            // console.log('RESULTADOO', result);
-            return result
+            console.log('holi', result)
+                // si la longitud de result es 0, entonces resolve links no encontrados
+                //  si no, resolvemos result
+            resolve(result)
         })
         .catch(error => {
             console.log('ERROOOR', error);
+            reject('readingfile catch')
+
         })
-    )
+        //)
 });
 
-// Use fsPromises.readFile() method
-// to read the file
+// readingFile(process.argv[2])
+//     .then((result) => {
+//         console.log("RESULT" + result);
+//     })
+//     .catch((error) => {
+//         console.log("ERROOOOOOOOOOOOR", error);
+//     });
 
-promesaPrueba(process.argv[2])
+
+const listLinks = function(links) {
+    links.forEach(link => console.log(link));
+}
+
+readingFile(process.argv[2])
     .then((result) => {
-        console.log("RESULT" + result);
+        const { links } = markdownLinkExtractor(result);
+
+        switch (process.argv[3]) {
+            case '--validate':
+                links.forEach(link => doHTTPRequest(process.argv[2], link));
+                break;
+            default:
+                listLinks(links);
+                break;
+        }
     })
     .catch((error) => {
         console.log("ERROOOOOOOOOOOOR", error);
@@ -30,3 +70,6 @@ promesaPrueba(process.argv[2])
 // module.exports = () => {
 //   // ...
 // };
+
+
+// node index.js "./files-md/example1.md" --validate
