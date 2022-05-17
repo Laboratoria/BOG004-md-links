77903598b04bd8662a699edd17adddd680bd0f6a
yuliduquedealba <yduquedealba@gmail.com>
2022-05-16 21:13:54 -0500

    Se comienza la implementación de los test, falla el 3ro y el 4to, los demás funcionan correctamente

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,15 +1,24 @@
 const { mdLinks } = require('../index.js');
-const {
-    getMdFiles,
-    readMdFiles,
-    getLinksMdFiles,
-    getObjetsLinks,
-    absolutePath,
-    extMdFile,
-    convertPath,
-    totalAndUnique,
-    broken,
-} = require('../functions.js');
+
+const route = "test//test-folder//test-file.md";
+const invalidRoute = "test//test-folder//test-file1.md";
+const fileWithoutLinks = "test//test-folder//anotherFiles1.md//hola.md";
+
+const arrayLinksValidated = [{
+        href: "https://www.google.com.co/",
+        text: "Google",
+        file: "C://Users//yduqu//OneDrive//Escritorio//Laboratoria//md-links//test//test-folder//test-file.md",
+        status: 200,
+        ok: "OK",
+    },
+    {
+        href: "https://www.googlee.com.coo/",
+        text: "Guugle",
+        file: "C://Users//yduqu//OneDrive//Escritorio//Laboratoria//md-links//test//test-folder//test-file1.md",
+        status: 404,
+        ok: "fail",
+    },
+];
 
 
 describe('mdLinks', () => {
@@ -20,13 +29,24 @@ describe('mdLinks', () => {
 });
 
 describe('mdLinks', () => {
-    const path = 'test/test-folder1.md';
 
     it('debe retornar promesa', () => {
-        expect(mdLinks(path) instanceof Promise).toBeTruthy();
+        expect(mdLinks(route) instanceof Promise).toBeTruthy();
     });
 
-    it('debe ser un array de objetos', () => mdLinks(path).then((res) => {
-        expect(res).toHaveLength(3);
-    }));
+    it("Debe retornar array de objetos de links validos", () =>
+        mdLinks(route, { validate: true }).then((e) =>
+            expect(e).toEqual(arrayLinksValidated)
+        ));
+
+    it('debe ser un string de ruta no valida', () =>
+        mdLinks(invalidRoute).then((e) =>
+            expect(e).toBe(arrayLinksValidated)
+        ));
+
+    it("Debe retornar mensaje de que el archivo no contiene links", () => {
+        mdLinks(fileWithoutLinks).catch((e) => {
+            expect(e).toMatch('El archivo no contiene Links')
+        });
+    });
 });
